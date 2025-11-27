0748d34f2ab1e5b6d71416fc4e3da8c30c3d574e
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-28 04:41:46 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import ProfileCard from './ProfileCard'
+import React, { useState } from 'react'
+
+
+
 
 const App = () => {
+
+const [isDark, setIsDark] = useState(false);
+
+function toggleTheme() {
+  setIsDark(!isDark);
+}
+
+
   return (
-    <div><ProfileCard/></div>
+  <div 
+  style={{
+    margin: 0,
+    backgroundColor: isDark? "black": "white",
+    color: isDark? "white": "black",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  }}
+   >
+
+    <h1>{isDark ? "Dark Mode": "Light Mode"}</h1>
+
+    <button onClick={toggleTheme} style={{
+      borderRadius: "10px", padding: "10px"
+    }} >
+      {isDark ? "Switch to Light Mode ☀️": "Switch to Dark Mode ✨"}
+    </button>
+
+  </div>
   )
 }
 
---
+++ src/Theme.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const [isDark, setIsDark] = useState(false);
+
+const Theme = () => {
+  return (
+    <>
+    setIsDark(!isDark);
+    </>
+  )
+}
+
+export default Theme
