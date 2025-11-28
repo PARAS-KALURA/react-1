6ed8fd3308f595ed4081a198d8475ebb05601544
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-28 20:34:07 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
  
-  const toDo = ["Coding", "Music", "Chess", "Gym"];
+  const [toDo, setToDo ] = useState([]);
+  const [input, setInput] = useState("");
+
+  function add() {
+    if(input.trim() === "") {
+      alert("Link to bsdk kuch");
+      return;
+    }
+
+
+    setToDo([...toDo, input]);
+    setInput("");
+  }
+
 
   return (
-    <div style={{
+    <div
+    style={{
       border: "2px solid gray",
       padding: "10px",
       borderRadius: "10px",
-    }} >
-      <h2>ToDo List</h2>
-      {toDo.map((list) => {
-         return <p>{list}</p>
+      textAlign: "center",
+    }}
+    >
+      <h2>To-Do List</h2>
+      <input
+      style={{
+        padding: "3px",
+        borderRadius: "5px",
+        outline: "none",
+        
+      }}
+      type="text" placeholder='Add item...'
+      value={input}
+      onChange={(e) => {
+        setInput(e.target.value)
+      }}
+      />
+
+      <button
+      style={{
+        backgroundColor: "red",
+        color: "white",
+        marginLeft: "10px",
+        borderRadius: "4px",
+        padding: "2.5px 5px",
+        border: "none"
+
+      }}
+      onClick={add}
+      >Add
+      </button>
+
+
+      {toDo.map((item,index) => {
+       return <p key={index}>{item}</p>
       })}
+
     </div>
   )
 }
 
 export default App
+ 
+
+
