9489ffb1559d2ded300b8fde946a888e0e781b46
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-12-01 23:49:30 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      gap: "21px",
+      gap: "22px",
       justifyContent: "center"
     }}>
 
